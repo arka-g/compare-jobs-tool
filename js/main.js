f9c70ec7577710e89344e5fb54160f4f338db962
Arka Ganguli <arkaganguli1@gmail.com>
2014-07-27 21:33:00 -0400

    rewrote hover functions
    not working for second time

--- js/main.js
+++ js/main.js
@@ -1,52 +1,22 @@
-function DropDown(el) {
-  console.log(el);
-  this.dd = el;
-  this.placeholder = this.dd.children('span');
-  this.opts = this.dd.find('ul.dropdown > li');
-  this.val = '';
-  this.index = -1;
-  this.initEvents();
-}
-DropDown.prototype = {
-  initEvents : function() {
-    var obj = this;
+//load DOM
+$(document).ready(function(){
+  $('.wrapper-dropdown-1').hover(function(){
+    //alert('clicked');
+    $(this).toggleClass('active');
 
-    obj.dd.on('click', function(event){
+    $(this).find('ul.dropdown>li').on('click',function(){
+      // $(this).toggleClass('active');
       $(this).toggleClass('active');
-      return false;
-    });
-
-    obj.opts.on('click',function(){
-      var opt = $(this);
-      obj.val = opt.text();
-      obj.index = opt.index();
-      obj.placeholder.text(obj.val);
       $(this).parent().parent().css("background","#4cbeff");
       $(this).parent().parent().css("color","#FFF");
+      var text = $(this).text();
+      $(this).parent().parent().text('You selected: ' + text);
+      //wtf this does not work..
+      $(this).removeClass('active');
+      console.log($(this));
     });
-  },
-  getValue : function() {
-    return this.val;
-  },
-  getIndex : function() {
-    return this.index;
-  }
-}
-
-$(function() { 
-        //how to find element that got clicked?
-
-        $('.wrapper-dropdown-1').on(
-          'click',function() {
-
-            var dd = new DropDown($(this));
-            console.log(dd);
-            $('.wrapper-dropdown-1').removeClass('active');
-          });
-
-      });
-
-
+  });
+});
 
 var $groupSvg
 , $groupPos
